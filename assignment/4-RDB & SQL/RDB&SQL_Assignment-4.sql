a3f59d27bffadd71d28cc2e921f29fc59db9b12c
safiye <sakindiye@.com>
2023-07-26 20:51:06 +0300

    upload rbd&sql assignment4

--- assignment/4-RDB & SQL/RDB&SQL_Assignment-4.sql
+++ assignment/4-RDB & SQL/RDB&SQL_Assignment-4.sql
@@ -50,9 +50,9 @@ quantity_comp INT NOT NULL,
 CREATE TABLE Supplier (
 supp_id INT NOT NULL,
 supp_name VARCHAR(50) NOT NULL,
-supp_location VARCHAR(50) NOT NULL,
-supp_country VARCHAR(50) NOT NULL,
-is_active bit NOT NULL,
+supp_location VARCHAR(50) NULL,
+supp_country VARCHAR(50) NULL,
+is_active BIT NOT NULL,
 	CONSTRAINT PK_supplier PRIMARY KEY (supp_id)
 );
 
@@ -68,3 +68,126 @@ quantity INT NOT NULL,
 );
 
 
+
+--ALTERNATIVE SOLUTION 1
+---------------------------------------
+CREATE DATABASE Manufacturer;
+GO
+USE Manufacturer;
+GO
+
+CREATE TABLE Product(
+prod_id INT IDENTITY (1, 1) PRIMARY KEY,
+prod_name VARCHAR(50) NOT NULL,
+quantity INT NOT NULL
+);
+
+
+CREATE TABLE Component (
+comp_id INT IDENTITY (1, 1) PRIMARY KEY,
+comp_name VARCHAR(50) NOT NULL,
+[description] VARCHAR(50) NOT NULL,
+quantity_comp INT NOT NULL	
+);
+
+CREATE TABLE Prod_Comp (
+prod_id INT NOT NULL,
+comp_id INT NOT NULL,
+quantity_comp INT NOT NULL,
+PRIMARY KEY (prod_id, comp_id),
+FOREIGN KEY (prod_id) REFERENCES [dbo].[Product](prod_id) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (comp_id) REFERENCES [dbo].[Component](comp_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Supplier (
+supp_id INT IDENTITY (1, 1) PRIMARY KEY,
+supp_name VARCHAR(50) NOT NULL,
+supp_location VARCHAR(50) NULL,
+supp_country VARCHAR(50) NULL,
+is_active BIT NOT NULL	
+);
+
+
+CREATE TABLE Comp_Supp (
+supp_id INT NOT NULL,
+comp_id INT NOT NULL,
+order_date DATE NOT NULL,
+quantity INT NOT NULL,
+PRIMARY KEY (supp_id, comp_id),
+FOREIGN KEY (supp_id) REFERENCES [dbo].[Supplier](supp_id) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (comp_id) REFERENCES [dbo].[Component](comp_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+--ALTERNATIVE SOLUTION 2
+---------------------------------------
+
+CREATE DATABASE Manufacturer;
+GO
+USE Manufacturer;
+GO
+
+CREATE TABLE Product(
+prod_id INT NOT NULL,
+prod_name VARCHAR(50) NOT NULL,
+quantity INT NOT NULL	
+);
+
+ALTER TABLE [dbo].[Product]
+ADD CONSTRAINT PK_product PRIMARY KEY (prod_id);
+
+
+
+
+CREATE TABLE Component (
+comp_id INT NOT NULL,
+comp_name VARCHAR(50) NOT NULL,
+[description] VARCHAR(50) NOT NULL,
+quantity_comp INT NOT NULL	
+);
+
+ALTER TABLE [dbo].[Component]
+ADD CONSTRAINT PK_component PRIMARY KEY (comp_id);
+
+
+
+CREATE TABLE Prod_Comp (
+prod_id INT NOT NULL,
+comp_id INT NOT NULL,
+quantity_comp INT NOT NULL	
+);
+
+ALTER TABLE [dbo].[Prod_Comp]
+ADD CONSTRAINT PK_prod_comp PRIMARY KEY (prod_id, comp_id),
+CONSTRAINT FK_product_pc FOREIGN KEY(prod_id) REFERENCES [dbo].[Product](prod_id), 
+CONSTRAINT FK_component_pc FOREIGN KEY(comp_id) REFERENCES [dbo].[Component](comp_id);
+
+
+
+
+CREATE TABLE Supplier (
+supp_id INT NOT NULL,
+supp_name VARCHAR(50) NOT NULL,
+supp_location VARCHAR(50) NULL,
+supp_country VARCHAR(50) NULL,
+is_active BIT NOT NULL	
+);
+
+ALTER TABLE [dbo].[Supplier]
+ADD CONSTRAINT PK_supplier PRIMARY KEY (supp_id);
+
+
+
+
+CREATE TABLE Comp_Supp (
+supp_id INT NOT NULL,
+comp_id INT NOT NULL,
+order_date DATE NOT NULL,
+quantity INT NOT NULL	
+);
+
+ALTER TABLE [dbo].[Comp_Supp]
+ADD CONSTRAINT PK_comp_supp PRIMARY KEY (supp_id, comp_id),
+CONSTRAINT FK_supplier_ccs FOREIGN KEY(supp_id) REFERENCES [dbo].[Supplier](supp_id), 
+CONSTRAINT FK_component_cs FOREIGN KEY(comp_id) REFERENCES [dbo].[Component](comp_id);
